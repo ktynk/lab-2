9bd724443dcbb76b3f7fe2b00a2a672a6f9c64c2
ktynk <ktyn26k@gmail.com>
2025-09-12 20:00:47 +0300

    Add files via upload

---
+++ 2 задание.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int x, y, R;
+	printf("введите координаты:");
+	scanf_s("%d %d",&x, &y);
+	printf("введите радиус:");
+	scanf_s("%d", &R);
+
+	if (pow(x, 2) + pow(y, 2) > pow(R, 2))
+		printf("нет");
+	else
+		printf("да");
+	return 0;
+
+   
+}
+
---
+++ 3 задание.cpp
@@ -0,0 +1,20 @@
+﻿
+#include<stdio.h>
+#include <locale.h>
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int x;
+	printf("Введите пару года по порядку под номером(пример:лето-1 и тд.)\n");
+	scanf_s("%d", &x);
+	switch (x)
+	{
+	case 1: printf("июнь,июль,август.\n"); break;
+	case 2: printf("сентябрь,октябрь,ноябрь.\n"); break;
+	case 3: printf("декабрь,январь,февраль.\n"); break;
+	case 4: printf("март,апрель,май.\n"); break;
+	default: printf("ИХ ВСЕГО 4.\n");
+	}
+	return 0;
+}
+		
