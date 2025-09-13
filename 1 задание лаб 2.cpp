cb93c3b7ce97549fc250de4080b2e38b28576273
ktynk <ktyn26k@gmail.com>
2025-09-13 16:14:09 +0300

    Update 1 задание лаб 2.cpp

--- 1 задание лаб 2.cpp
+++ 1 задание лаб 2.cpp
@@ -3,7 +3,7 @@
 #include <locale.h>
 int main() {
     setlocale(LC_ALL, "Russian");
-    double x1, x2, x3, y1, y2, y3, AB, BC, CA;
+    double x1, x2, x3, y1, y2, y3, AB, BC, CA,a,b,c,cos;
     printf("введите координаты по порядку(x1 и y1):");
     scanf_s("%lf %lf ", &x1, &y1);
     printf("введите координаты по порядку(x2 и y2):");
@@ -16,20 +16,28 @@ int main() {
     AB = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
     CA = sqrt(pow((x3 - x1), 2) + pow((y3 - y1), 2));
     BC = sqrt(pow((x3 - x2), 2) + pow((y3 - y2), 2));
+    if (AB>BC && AB>CA){
+        c=AB;
+        a=CA;
+        b=BC;
+    } else if( BC>CA && BC>AB){
+        c=BC;
+        a=AB;
+        b=CA;
+    }
+    else{
+        c=CA;
+        a=BC;
+        b=AB;
+    }
 
-    double sides[3] = { AB, BC, CA };
-    std::sort(sides, sides 3);
-    double a = sides[0];   // катет  
-  double b = sides[1];   // катет  
-  double c = sides[2];   // гипотенуза
-  double a2 = a * a, b2 = b * b, c2 = c * c;
-
-    if ((a2 + b2) == c2)
+    if ((pow(a,2) + pow(b,2) == pow(c,2))
         printf("Прямоугольный треугольник");
-    else if (a2 + b2 > c2)
+    else if ( pow(+a,2) + pow(b,2) >pow(c,2))
         printf("Остроугольный треугольник");
     else
         printf("Тупоугольный треугольник");
     return 0;
 }
+
 
